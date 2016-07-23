6257f34df7ae3849de4623b3f6543865734babf4
Hector Valdez Castro <hvaldez@hvc-macmini.local>
2016-07-23 18:41:13 -0500

    Initial Commit

---
+++ Curso/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Curso
+//
+//  Created by Hector Valdez Castro on 23/07/16.
+//  Copyright © 2016 Hector Valdez Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CursoTests/CursoTests.swift
@@ -0,0 +1,36 @@
+//
+//  CursoTests.swift
+//  CursoTests
+//
+//  Created by Hector Valdez Castro on 23/07/16.
+//  Copyright © 2016 Hector Valdez Castro. All rights reserved.
+//
+
+import XCTest
+@testable import Curso
+
+class CursoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
